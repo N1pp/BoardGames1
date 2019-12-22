3124c3df961d270ce7ae9a6711b1a68ae6b587cf
N1pp <dimanepovin00@gmail.com>
2019-12-22 22:11:15 +0400

    design changes

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -35,30 +35,34 @@
         </form>
     </div>
     <div style="display:flex; flex-direction: row; flex-wrap: wrap; justify-content: center">
-        @foreach($products as $product)
-            <div class="card" style="width: 18rem; margin: 1rem; background: yellowgreen">
-                {{--        <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">--}}
-                <div class="card-body" >
-                    <div style="display: flex; flex-wrap: nowrap">
-                        <div style="display:flex; flex-direction: row">
-                            <h5 class="card-title">{{$product->name}}</h5>
+        @if($products->get(0))
+            @foreach($products as $product)
+                <div class="card" style="width: 18rem; margin: 1rem; background: yellowgreen">
+                    {{--        <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">--}}
+                    <div class="card-body">
+                        <div style="display: flex; flex-wrap: nowrap">
+                            <div style="display:flex; flex-direction: row">
+                                <h5 class="card-title">{{$product->name}}</h5>
+                            </div>
+                            <div style="display:flex; flex-direction: row-reverse">
+                                <p class="card-title" style="margin:0 2rem">{{$product->price}}</p>
+                            </div>
                         </div>
-                        <div style="display:flex; flex-direction: row-reverse">
-                            <p class="card-title" style="margin:0 2rem">{{$product->price}}</p>
-                        </div>
+                        <p class="card-text">{{$product->descriptionSmall}}</p>
                     </div>
-                    <p class="card-text">{{$product->descriptionSmall}}</p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item" style="background: yellowgreen">
+                            Tags:@foreach($product->tags()->get() as $tag){{$tag->value}} @endforeach</li>
+                        <li class="list-group-item" style="background: yellowgreen">Amount of
+                            comments:{{$product->comment}}</li>
+                        <li class="list-group-item" style="background: yellowgreen">Rating: {{$product->rate}}</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="{{ route('product',[$product]) }}" class="card-link">Go to game page</a>
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item" style="background: yellowgreen">
-                        Tags:@foreach($product->tags()->get() as $tag){{$tag->value}} @endforeach</li>
-                    <li class="list-group-item" style="background: yellowgreen">Amount of comments:{{$product->comment}}</li>
-                    <li class="list-group-item" style="background: yellowgreen">Rating: {{$product->rate}}</li>
-                </ul>
-                <div class="card-body">
-                    <a href="{{ route('product',[$product]) }}" class="card-link">Go to game page</a>
-                </div>
-            </div>
-        @endforeach
+            @endforeach
+        @else <h1>No products match your request</h1>
+        @endif
     </div>
 @endsection
